b4723cffd0e6d25260e2c1f74024da21d721f33b
Matthew Lee <ml7@uw.edu>
2014-12-03 14:08:50 -0800

    Modified maketrans() arguments to create the desired translation.

--- Students/MatLee/session03/rot13.py
+++ Students/MatLee/session03/rot13.py
@@ -1,9 +1,14 @@
 from string import maketrans
 
 def rot13(input):
+    """Return a translation of a string, input.
+    A cyclical shift of 13 alphabetical places for letters only, 
+    preserving white space, case, and punctuation."""
     start = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    finish = "AB1DEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    finish = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
     translation = maketrans(start, finish)
     return input.translate(translation)
 
-print rot13("Check.")
+if __name__ == "__main__":
+    assert (rot13("ABCDEFGHIJKLMNOPQRSTUVWXYZ. abcdefghijklmnopqrstuvwxyz")
+            == "NOPQRSTUVWXYZABCDEFGHIJKLM. nopqrstuvwxyzabcdefghijklm")
