d77402d0acf46d77a07282fef5734ed99661fca0
Matthew Lee <ml7@uw.edu>
2014-12-03 14:50:12 -0800

    Modified function to be able to take unicode strings as input.

--- Students/MatLee/session03/rot13.py
+++ Students/MatLee/session03/rot13.py
@@ -4,11 +4,16 @@ def rot13(input):
     """Return a translation of a string, input.
     A cyclical shift of 13 alphabetical places for letters only, 
     preserving white space, case, and punctuation."""
+    input = str(input)
     start = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     finish = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
     translation = maketrans(start, finish)
-    return input.translate(translation)
+    return unicode(input.translate(translation))
+
 
 if __name__ == "__main__":
-    assert (rot13("ABCDEFGHIJKLMNOPQRSTUVWXYZ. abcdefghijklmnopqrstuvwxyz")
-            == "NOPQRSTUVWXYZABCDEFGHIJKLM. nopqrstuvwxyzabcdefghijklm")
+    #Verify that the returned string is in unicode.
+    print "%r" % rot13("Check.")
+    #Verify that the function performs as desired.
+    assert (rot13(u"ABCDEFGHIJKLMNOPQRSTUVWXYZ. abcdefghijklmnopqrstuvwxyz123")
+            == u"NOPQRSTUVWXYZABCDEFGHIJKLM. nopqrstuvwxyzabcdefghijklm123")
