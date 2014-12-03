27904fa588faabdc5c885aaa4d214226dff04209
Matthew Lee <ml7@uw.edu>
2014-12-03 15:19:35 -0800

    Finished list_lab.py series 1.

--- Students/MatLee/session03/list_lab.py
+++ Students/MatLee/session03/list_lab.py
@@ -1,2 +1,23 @@
-the_list = ["Apples", "Pears", "Oranges", "Peaches"]
-the_list
+#!/user/bin/env python
+
+def number():
+    return 0
+    
+the_list = [u"Apples", u"Pears", u"Oranges", u"Peaches"]
+print the_list
+
+the_list.append(unicode(raw_input(u"Input another fruit to add to this list: ").title()))
+print the_list
+
+print the_list[number()]
+
+the_list = [u"Pomegranate"] + the_list
+print the_list
+the_list.insert(0, u"Pomelo")
+print the_list
+
+for fruit in the_list:
+    if fruit[0] == u"P":
+        print fruit,
+else:
+    print "\n"
