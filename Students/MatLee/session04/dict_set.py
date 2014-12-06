2da3c28fc8c02071a2973e0c160a90bd1f3bbf3a
Matthew Lee <ml7@uw.edu>
2014-12-05 16:06:12 -0800

    Completed first series of tasks with the created dictionary.

--- Students/MatLee/session04/dict_set.py
+++ Students/MatLee/session04/dict_set.py
@@ -1,3 +1,13 @@
 stuff = {"name": "Chris", 
          "city": "Seattle", 
          "cake": "Chocolate"}
+
+print stuff
+stuff.pop("cake")
+print stuff
+stuff.setdefault("fruit", "Mango")
+print stuff.keys()
+a = stuff.values()
+print a
+print "cake" in stuff
+print "Mango" in a
