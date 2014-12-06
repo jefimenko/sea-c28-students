3d91e5b008c843809e5ff53e9b833164ff020c10
Matthew Lee <ml7@uw.edu>
2014-12-05 16:33:51 -0800

    Completed second series of operations on the first dictionary.

--- Students/MatLee/session04/dict_set.py
+++ Students/MatLee/session04/dict_set.py
@@ -11,3 +11,12 @@ a = stuff.values()
 print a
 print "cake" in stuff
 print "Mango" in a
+
+
+things = stuff.items()
+for b in range(len(things)):  
+    things[b] = (things[b][0], things[b][1].count('a'))
+
+stuff_prime = dict(things)
+print stuff
+print stuff_prime
