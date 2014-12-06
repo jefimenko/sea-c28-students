a0d8c8b2a207337a56f08261a002124df6fee89c
Matthew Lee <ml7@uw.edu>
2014-12-05 17:29:01 -0800

    Complete set operations.

--- Students/MatLee/session04/dict_set.py
+++ Students/MatLee/session04/dict_set.py
@@ -12,11 +12,10 @@ print a
 print "cake" in stuff
 print "Mango" in a
 
-b = range(16)
 c = []
-for num in b:
+for num in range(16):
     c.append(hex(num)[2:])
-dec_hex = dict(zip(b, c))
+dec_hex = dict(zip(range(16), c))
 print dec_hex
 
 
@@ -24,6 +23,37 @@ things = stuff.items()
 for b in range(len(things)):  
     things[b] = (things[b][0], things[b][1].count('a'))
 
-stuff_prime = dict(things)
+stuff_a = dict(things)
 print stuff
-print stuff_prime
+print stuff_a
+
+
+s2 = set()
+s3 = set()
+s4 = set()
+
+for num in range(1,21):
+    if num % 2 is 0:
+        s2.add(num)
+    if num % 3 is 0:
+        s3.add(num)
+    if num % 4 is 0:
+        s4.add(num)
+else:
+    print s2
+    print s3
+    print s4
+
+print s3.issubset(s2)
+print s4.issubset(s2)
+
+
+letters = set('Python'[:])
+letters.add('i')
+print letters
+
+frozen = frozenset('marathon'[:])
+print frozen
+
+print letters.union(frozen)
+print letters.intersection(frozen)
