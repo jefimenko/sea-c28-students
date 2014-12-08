73cac973791ccd952571a2c8aadd986868765909
Matthew Lee <ml7@uw.edu>
2014-12-08 14:32:16 -0800

    Added safe_input() for use in place of raw_input().

--- Students/MatLee/session04/mailroom.py
+++ Students/MatLee/session04/mailroom.py
@@ -3,9 +3,15 @@ def main():
         print '(1) Send a Thank You.'
         print '(2) Create a Report.'
         print '(3) Exit program.'
-        a = raw_input('Which would you like to do? ')
+        a = safe_input('Which would you like to do? ')
         if 'exit program' in a.lower():
             end()
+
+def safe_input(display=""):
+    try:
+        return raw_input(display)
+    except (EOFError, KeyboardInterrupt):
+        return None
 
 def end():
     quit()
