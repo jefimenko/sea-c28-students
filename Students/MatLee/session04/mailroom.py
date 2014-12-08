9443e4994f4d7f796393d7a073116b1cd417ca01
Matthew Lee <ml7@uw.edu>
2014-12-08 15:06:49 -0800

    Added placeholders for functions.

--- Students/MatLee/session04/mailroom.py
+++ Students/MatLee/session04/mailroom.py
@@ -1,3 +1,5 @@
+HISTORY = 'mailroom.txt'
+
 def main():
     while True:
         print '(1) Send a Thank You.'
@@ -6,12 +8,28 @@ def main():
         a = safe_input('Which would you like to do? ')
         if 'exit program' in a.lower():
             end()
+        elif 'create a report' in a.lower():
+            report()
+        elif 'send a thank you' in a.lower():
+            send()
+
 
 def safe_input(display=""):
     try:
         return raw_input(display)
     except (EOFError, KeyboardInterrupt):
         return None
+
+
+def send():
+    print "thank you!"
+    return
+
+
+def report():
+    print "report"
+    return
+
 
 def end():
     quit()
