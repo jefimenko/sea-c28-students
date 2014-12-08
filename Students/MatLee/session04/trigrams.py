4a39c98a4502f8fc6db07469ce795e622ee36946
Matthew Lee <ml7@uw.edu>
2014-12-07 21:22:21 -0800

    Formed initial way to analysis the text using dictionary to store results. To-do: multiple third-word entries for each pair of words.

--- Students/MatLee/session04/trigrams.py
+++ Students/MatLee/session04/trigrams.py
@@ -3,18 +3,30 @@ from io import open
 with open('sherlock_small.txt') as f:
     out = f.read()
 
-print f.closed
-print out
+# Split the text assuming only '--', white space, 
+# or '\n' are used to delimit words. Punctuation is included in analysis.
 out = out.split('--')
-print out
-
 
 for chunk in out[:]:
     out.remove(chunk)
     out += chunk.split()
 
-print repr(out)
+analysis = dict()
 
-#for word in out:
-#    print repr(word)
+# Create auxiliary symbol that will hold two words
+# previous to current entry in data.
+# Print out the list for files two or less words in length.
+try:
+    temp = [out[0], out[1]]
+    out[2]
+except IndexError:
+    print temp
 
+for word in out[2:]:
+    analysis.setdefault((temp[0], temp[1]), word)
+    temp[0], temp[1] = temp[1], temp[0]
+    print temp
+    temp[1] = word
+    print temp
+
+print analysis
