25ee72663136e1bf8f92cfbb09118299966b330e
Matthew Lee <ml7@uw.edu>
2014-12-09 16:58:38 -0800

    Reduced function_builder() to one line using list comprehension.

--- Students/MatLee/session05/lambda_magic.py
+++ Students/MatLee/session05/lambda_magic.py
@@ -1,14 +1,2 @@
 def function_builder(n):
-
-    l = []
-    for s in range(n):
-        l.append(lambda a, b=s: a + b)
-    return l
-
-
-c = function_builder(12)
-print c[0](1)
-print c[11](12)
-
-for b in c:
-    print b(3)
+    return [lambda a, b=s: a + b for s in range(n)]
