ff971f399dd7f8a2f1b164604a8099247f3eac25
Matthew Lee <ml7@uw.edu>
2014-12-10 00:00:33 -0800

    Created a set that holds all values in s2, s3, and s4.

--- Students/MatLee/session05/dict_set_comp.py
+++ Students/MatLee/session05/dict_set_comp.py
@@ -12,21 +12,21 @@ print stuff.format(**food_prefs)
 # Create dictionary of numbers, as keys, and hexadecimal equivalents.
 # Hex values are represented as strings, omitting leading characters.
 d = dict([(x, hex(x)[2:]) for x in range(16)])
-print 'd'
-print d
 
 j = {x: hex(x)[2:] for x in range(16)}
-print 'j'
-print j
 
-
-food_prefs = {thing: specific.count('a')+specific.count('A') for thing, specific in food_prefs.iteritems()}
-print food_prefs
+# Using dictionary comprehension, modify values in food_prefs to the number of
+# 'a's in the value.
+food_prefs = {thing: specific.count('a') + specific.count('A') for thing, specific in food_prefs.iteritems()}
 
 
 s2 = {x for x in range(21) if not (x % 2)}
-print s2
 s3 = {x for x in range(21) if not (x % 3)}
-print s3
 s4 = {x for x in range(21) if not (x % 4)}
-print s4
+
+
+s_all = set()
+for i in range(2, 5):
+    s_all = s_all.union({x for x in range(21) if not (x % i)})
+print s_all
+
