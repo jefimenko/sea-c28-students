033a5f7860594547bcfa3bf933e7b4481c5494f2
Matthew Lee <ml7@uw.edu>
2014-12-09 23:24:01 -0800

    Created dictionary using list comprehension.

--- Students/MatLee/session05/dict_set_comp.py
+++ Students/MatLee/session05/dict_set_comp.py
@@ -8,3 +8,8 @@ food_prefs = {u'name': u'Mat',
 stuff = (u'{name} lives in Bellevue. The cake was {cake},'
          + u'so he decided to add {fruit} and {pasta} to his {salad} salad.')
 print stuff.format(**food_prefs)
+
+# Create dictionary of numbers, as keys, and hexadecimal equivalents.
+# Hex values are represented as strings, omitting leading characters.
+d = dict([(x, hex(x)[2:]) for x in range(16)])
+print d
