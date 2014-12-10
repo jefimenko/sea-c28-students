09bd6df02287e9194b508fde5d5dd939c90a5dfa
Matthew Lee <ml7@uw.edu>
2014-12-10 00:35:49 -0800

    Added demonstration of desired behavior for function_builder() in a if __name__ == ... block.

--- Students/MatLee/session05/lambda_magic.py
+++ Students/MatLee/session05/lambda_magic.py
@@ -1,2 +1,7 @@
 def function_builder(n):
     return [lambda a, b=s: a + b for s in range(n)]
+
+if __name__ == "__main__":
+    a = function_builder(10)
+    for c in range(3):
+        print [i(c) for i in a]
