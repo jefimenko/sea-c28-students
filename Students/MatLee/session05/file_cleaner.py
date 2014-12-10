6fb08f900431ba70219358c7c2cd52b8a30c1a9b
Matthew Lee <ml7@uw.edu>
2014-12-09 23:00:23 -0800

    Finished output function with option to overwrite old file or create a new file.

--- Students/MatLee/session05/file_cleaner.py
+++ Students/MatLee/session05/file_cleaner.py
@@ -1,6 +1,28 @@
 import io
 import sys
 
+
+def save_file(copy, f):
+    f.close()
+    while True:
+        a = raw_input(u'Would you like to overwrite existing file,\n'
+                      u'or create a new file? ')
+        if u'overwrite' in a.lower():
+            writer(filename, copy)
+            return
+        elif u'create a new file' in a.lower():
+            b = raw_input(u'Please name the new file: ')
+            writer(b + '.txt', copy)
+            return
+
+
+def writer(name, copy):
+    f = open(name, 'w')
+    f.write('\n'.join(copy))
+    f.close()
+    return
+
+    
 try:
     filename = sys.argv[1]
 except IndexError:
@@ -8,10 +30,16 @@ except IndexError:
     quit()
 
 try:
-    f = open(filename)
+    f = open(filename, 'r')
 except IOError:
     f.close()
+    print f.closed
 
-print f.closed   
+
+copy = []
+for line in f:
+    copy.append(line.strip())
+
+save_file(copy, f)
+
 f.close()
-print f.closed
