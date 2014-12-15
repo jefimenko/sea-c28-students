22070080981474b6146d18df3a384d3949c744fa
Matthew Lee <ml7@uw.edu>
2014-12-15 12:42:32 -0800

    Changed Element to inherit object and include a list in composition, rather than inherit from list.

--- Students/MatLee/session06/html_render.py
+++ Students/MatLee/session06/html_render.py
@@ -1,12 +1,29 @@
-class Element(list):
-    opening_tag = u'<>'
-    closing_tag = u'</>'
+class Element(object):
+    opening_tag = u''
+    closing_tag = u''
+    
+    def __init__(self):
+        self.content = []
 
     def render(self, file_out, ind=u''):
-        content = [ind + item for item in self]
+        display = []
+        for item in self.content:
+            if isinstance(item, Element):
+                display += item.render(item, file_out, ind+=ind).split('\n')
+            else:
+                display.append(ind + item)
 
-        all_out = [self.opening_tag] + content + [self.closing_tag]
+        all_out = [self.opening_tag] + display + [self.closing_tag]
         print u'\n'.join(all_out)
+        return display
+
+    def append(self, item):
+        self.content.append(item)
+
+
+class Html(Element):
+    opening_tag = u'<html>'
+    closing_tag = u'</html>'
 
 
 class Body(Element):
