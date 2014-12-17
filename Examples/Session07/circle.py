627bfe5321f77e8b197500af5aebf749e3a87a89
Matthew Lee <ml7@uw.edu>
2014-12-17 11:37:52 -0800

    Circle now passes for test_circle1.py.

--- Examples/Session07/circle.py
+++ Examples/Session07/circle.py
@@ -7,4 +7,19 @@ import math
 
 
 class Circle(object):
-    pass
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_diameter(self):
+        return self.radius * 2.0
+    def set_diameter(self, diameter=None):
+        if diameter:
+            self.radius = diameter / 2.0
+        else:
+            self.diameter = self.radius * 2.0
+    diameter = property(get_diameter, set_diameter)
+    
+    def get_area(self):
+        return math.pi * (self.radius**2)
+    area = property(get_area)
