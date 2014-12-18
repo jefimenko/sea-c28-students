cd755fefda798a307994d9d9eefeed44f59f16b0
Matthew Lee <ml7@uw.edu>
2014-12-18 14:42:17 -0800

    Modified __init__ for IterateMe_2 to validate parameters.

--- Examples/Session08/iterator_1.py
+++ Examples/Session08/iterator_1.py
@@ -32,12 +32,34 @@ class IterateMe_2(object):
     ( like xrange(4) )
     """
     def __init__(self, start, stop, step=1):
+        # Prevent invalid parameter input
+        if not (isinstance(start, int)
+                and isinstance(stop, int)
+                and isinstance(step, int)):
+            raise TypeError
+
+        # Prevent step of size 0
+        if not step:
+            raise ValueError
+
+        # Ensure valid range for positive step size
+        if step > 0:
+            if start > stop:
+                self.stop = start
+            else:
+                self.stop = stop
+        # Ensure valid range for negative step size
+        else:
+            if (start < stop):
+                self.start = stop
+            else:
+                self.start = start
+        self.step = step
         self.current = start - 1
-        self.stop = stop
     def __iter__(self):
         return self
     def next(self):
-        self.current += 1
+        self.current += self.step
         if self.current < self.stop:
             return self.current
         else:
@@ -54,11 +76,10 @@ if __name__ == "__main__":
     print u'second version'
     it = IterateMe_2(2,20,2)
     for i in it:
-        if i > 10: break
         print i
-
+        if i > 10: break
+    # Check behaviour upon resuming iteration.
     print u'break'
 
     for i in it:
         print i
-
