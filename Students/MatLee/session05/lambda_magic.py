2132b372e60d5c03b6872620ca6d8c419370dd39
Matthew Lee <ml7@uw.edu>
2014-12-19 10:23:07 -0800

    Updated test for lambda_magic.py to use an assert statement.

--- Students/MatLee/session05/lambda_magic.py
+++ Students/MatLee/session05/lambda_magic.py
@@ -4,4 +4,4 @@ def function_builder(n):
 if __name__ == "__main__":
     a = function_builder(10)
     for c in range(3):
-        print [i(c) for i in a]
+        assert [c+place for place in xrange(len(a))] == [i(c) for i in a]
