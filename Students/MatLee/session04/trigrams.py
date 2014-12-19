f0dc34fb0b62325680d2ae2d8e85b7f946a97f4c
Matthew Lee <ml7@uw.edu>
2014-12-19 11:11:06 -0800

    Rewrote initial data preparation as functions.

--- Students/MatLee/session04/trigrams.py
+++ Students/MatLee/session04/trigrams.py
@@ -2,22 +2,32 @@ from io import open
 import random
 
 #FILE_NAME = 'pg28659.txt'
-#FILE_NAME = 'sherlock_small.txt'
-FILE_NAME = 'sherlock.txt'
+FILE_NAME = 'sherlock_small.txt'
+#FILE_NAME = 'sherlock.txt'
 
-with open(FILE_NAME) as f:
-    data = f.read()
+def read_data(file_name)
+    """
+    Open a file and separate into 'chunks' delimited by '\n' characters.
+    Return a list of the 'chunks'.
+    """
+    with open(file_name) as f:
+        data = f.read()
+    # Split the text assuming only white space is used to delimit words.
+    # Punctuation is included in analysis and two consecutive \n characters
+    # are interpreted as paragraph separations.
+    data = data.split('\n')
+    return data
 
-# Split the text assuming only white space is used to delimit words.
-# Punctuation is included in analysis and two consecutive \n characters
-# are interpreted as paragraph separations.
-
-data = data.split('\n')
-
-for chunk in data[:]:
-    data.remove(chunk)
-    data += chunk.split()
-    data.append('\n')
+def chunker(data):
+    """
+    Break 'chunks' into individual words, and reinsert '\n' characters
+    after each 'chunk'.
+    """
+    for chunk in data[:]:
+        data.remove(chunk)
+        data += chunk.split()
+        data.append('\n')
+    return data
 
 
 analysis = dict()
@@ -55,3 +65,6 @@ while analysis.has_key((temp[0], temp[1])):
     elif n is temp[1]:
         print n
     temp[0], temp[1] = temp[1], n
+
+data = read_data(FILE_NAME)
+data = chunker(data)
