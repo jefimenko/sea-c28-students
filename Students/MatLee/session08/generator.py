80f370c5e2f62c1dc7cb3df56b275fbcb33e5cbf
Matthew Lee <ml7@uw.edu>
2014-12-18 19:53:51 -0800

    Updated intsum() and doubler() to truly be generators.

--- Students/MatLee/session08/generator.py
+++ Students/MatLee/session08/generator.py
@@ -1,39 +1,30 @@
 # Sum of integers
-class intsum(object):
-    """
-    Produces the cumulative sum of integers using intsum.next().
-    Start from 0, adding incrementally larger numbers.
-    """
-    def __init__(self):
-        self.current = 0
-        self.sum = 0
-
-    # Increment and sum values accordingly and return self.sum before addition
-    # of current value.
-    def next(self):
-        self.current += 1
-        self.sum += self.current
-        return self.sum - self.current
+# Yield current value, then at the following next()
+# call, update values, and then yield updated sum value.
+def intsum():
+    current = 0
+    sum = 0
+    while True:
+        yield sum
+        current += 1
+        sum += current
 
 
 # Doubler
-class doubler(object):
-    """
-    Produce results of 2 raised to the power of n, where
-    2^0 is defined as 1, and used as the first case.
-    """
-    def __init__(self):
-        self.info = [1, 1]
-
-    # Calculate result for next call while preserving and returning
-    # current result
-    def next(self):
-        self.info[0], self.info[1] = self.info[1], self.info[1]*2
-        return self.info[0]
+def doubler():
+    current = 1
+    while True:
+        yield current
+        current *= 2
 
 # Fibonacci sequence
-class fib(object):
-    pass
+def fib():
+    info = [1, 1]
+
+    while True:
+        yield info[0]
+        info[0], info[1] = info[1], info[0] + info[1]
+
 
 # Prime numbers
 class prime(object):
