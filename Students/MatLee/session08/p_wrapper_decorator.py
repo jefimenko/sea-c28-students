1df48af0a0cbeb2267784eae74e88099c7008a2f
Matthew Lee <ml7@uw.edu>
2014-12-18 20:58:45 -0800

    Completed p_wrapper function.

---
+++ Students/MatLee/session08/p_wrapper_decorator.py
@@ -0,0 +1,16 @@
+def p_wrapper(func):
+    """
+    Takes a function that returns a string and wraps it in paragraph
+    tags using string formatting.
+    """
+    def inner(text):
+        return u'<p> {0} </p>'.format(func(text))
+    return inner
+
+@p_wrapper
+def return_a_string(text):
+    # Check if the given input is a string.
+    if not isinstance(text, str):
+        raise TypeError
+    # Make sure the input given is passed on in unicode.
+    return unicode(text)
