2e82a35ba6ebf3c2bd8c0c5ff7789293662c48cd
Matthew Lee <ml7@uw.edu>
2014-12-28 20:05:35 -0800

    Modified __init__ to make use of class tag information stored as tuples, and updated render() to use a new recursive function content_former().

--- Students/MatLee/session06/html_render.py
+++ Students/MatLee/session06/html_render.py
@@ -5,59 +5,55 @@ class Element(object):
 
     # Construct an html element.
     # Keyword arguments are interpreted and rendered as
-    # [key]="values" for each argument given.
+    # key="values" for each argument given.
     def __init__(self, text=u'', tag=u'', **kwargs):
+        self.self_closing = not bool(self.closing_tag)
         self.opening_tag = list(self.opening_tag)
-        self.closing_tag = list(self.closing_tag)
+        if not self.self_closing:
+            self.closing_tag = list(self.closing_tag)
         self.content = []
         if text:
             self.content.append(text)
-        
 
         if kwargs:
-            self_closing = not bool(self.closing_tag)
-            if self_closing:
-                self.opening_tag.insert(2, kwargs)
-        self.opening_tag[1] = tag
-        self.closing_tag[1] = tag
+            for key, value in kwargs.items():
+                self.opening_tag.insert(-1, u' {0}="{1}"'.format(key, value))
 
-            # if self_closing:
-                # self.opening_tag = self.opening_tag[:-3]
-            # else:
-                # self.opening_tag = self.opening_tag[:-1]
-            # for key, value in kwargs.iteritems():
-                # self.opening_tag += u' {style}="{options}"'.format(
-                    # style=key,
-                    # options=value)
-            # if self_closing:
-                # self.opening_tag += u' />'
-            # else:
-                # self.opening_tag += u'>'
+        self.opening_tag[1] = tag
+        if self.self_closing:
+            self.opening_tag[-1] = u' />'
+        if not self.self_closing:
+            self.closing_tag[1] = tag
+
 
     # Render information in html element in a nicely formatted way.
-    # Indentation is determined by keyword argument 'ind', while new_line
-    # allows an element to be rendered in one line if given an empty string.
-    def render(self, file_out, depth=1, display=[]):
-        display.append(self.ind * (depth - 1) + u''.join(self.opening_tag))
+    # Indentation is determined by global symbol 'ind'.
+    def render(self, file_out):
+        file_out.write(u'\n'.join(self.content_former()))
 
+    # Return a list containing content with appropriate indentation.
+    def content_former(self, depth=1):
+        display = []
+        display.append(self.ind * (depth - 1) + u''.join(self.opening_tag))
         for item in self.content:
+            # Grow the list by appending individual items of content.
+            # If an item is an element, that item's content will be fetched and
+            # used to grow the display list before this current loop continues
+            # to another item.
             try:
-                item.render(file_out, depth + 1)
+                current = item.content_former(depth)
+                for inner in current:
+                    try:
+                        inner.content_former(depth+1)
+                    except AttributeError:
+                        display.append(self.ind * depth + inner)
+                    
             except AttributeError:
                 display.append(self.ind * depth + item)
-        display.append(self.ind * (depth - 1) + u''.join(self.closing_tag))
 
-        if depth == 1:
-            file_out.write(u'\n'.join(display))
-
-        # file_out.write( new_line + ind + self.opening_tag )
-        # for item in self.content:
-            # if isinstance(item, Element):
-                # item.render(file_out, ind, depth+1)
-            # else:
-                # file_out.write( new_line + ind*depth+item )
-
-        # file_out.write( new_line + ind*(depth-1) + self.closing_tag )
+        if not self.self_closing:
+            display.append(self.ind * (depth - 1) + u''.join(self.closing_tag))
+        return display
 
     def append(self, item):
         self.content.append(item)
@@ -71,7 +67,6 @@ class Html(Element):
     def render(self, file_out):
         file_out.write(u'<!DOCTYPE html>\n')
         Element.render(self, file_out)
-
 
 class Body(Element):
 
@@ -99,60 +94,52 @@ class Li(Element):
         Element.__init__(self, text, tag=u'li', **kwargs)
 
 
-class OneLineTag(Element):
 
+class OneLineTag(Element):
     # Render an element in a single line,
     # even if element contains more than one string.
     # Assumes there are no tags in content.
-    def render(self, file_out, ind=u'', depth=1):
-        file_out.write(u'\n' + ind*(depth-1))
-        Element.render(self, file_out, ind=u'', depth=1, new_line=u'')
+    def content_former(self, depth=1):
+        one_liner = Element.content_former(self, depth)
+        for place, item in enumerate(one_liner[1:]):
+            one_liner[place + 1] = item.strip()
+        display = [u''.join(one_liner)]
+        return display
 
 class Title(OneLineTag):
-    # opening_tag[1] = u'title'
-    # closing_tag[1] = u'title'
-    pass
+
+    def __init__(self, content):
+        Element.__init__(self, text=content, tag=u'title')
 
 class A(OneLineTag):
-    # opening_tag[1] = u'a'
-    # closing_tag[1] = u'a'
 
     def __init__(self, link, content):
-        Element.__init__(self, text=content, href=link)
+        Element.__init__(self, text=content, tag=u'a', href=link)
 
 class H(OneLineTag):
-    # opening_tag[1] = u'<h'
-    # closing_tag[1] = u'</h'
 
-    # def __init__(self, size, content):
-        # Element.__init__(self, content)
-        # self.opening_tag += str(size)+u'>'
-        # self.closing_tag += str(size)+u'>'
-    pass
+    def __init__(self, size, content):
+        Element.__init__(self, content, tag=u'h'+str(size))
+
 
 
 class SelfClosingTag(Element):
     closing_tag = None
 
     def __init__(self, text=u'', **kwargs):
-        Element.__init__(self, text, tag=u'html', **kwargs)
-
-    def render(self, file_out, ind=u'', depth=1):
-        file_out.write( u'\n' + ind*(depth-1) + self.opening_tag )
-        for item in self.content:
-            file_out.write(item)
+        Element.__init__(self, text, tag=u'', **kwargs)
 
 class Hr(SelfClosingTag):
 
     def __init__(self, text=u'', **kwargs):
-        Element.__init__(self, text, tag=u'html', **kwargs)
+        Element.__init__(self, text, tag=u'hr', **kwargs)
 
 class Br(SelfClosingTag):
 
     def __init__(self, text=u'', **kwargs):
-        Element.__init__(self, text, tag=u'html', **kwargs)
+        Element.__init__(self, text, tag=u'br', **kwargs)
 
 class Meta(SelfClosingTag):
 
     def __init__(self, text=u'', **kwargs):
-        Element.__init__(self, text, tag=u'html', **kwargs)
+        Element.__init__(self, text, tag=u'meta', **kwargs)
